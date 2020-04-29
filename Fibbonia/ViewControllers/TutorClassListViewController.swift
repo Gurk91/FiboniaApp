68b17ff7ba10f718d7bd926d7e19cc68c3d168dd
Gurkarn Goindi <gurkarn.goindi@berkeley.edu>
2020-04-29 02:17:23 +0000

    display classes added

--- Fibbonia/ViewControllers/TutorClassListViewController.swift
+++ Fibbonia/ViewControllers/TutorClassListViewController.swift
@@ -7,24 +7,44 @@
 //
 
 import UIKit
+import Firebase
 
-class TutorClassListViewController: UIViewController {
+class TutorClassListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        tableView.reloadData()
+        data = currTutor.classes
+    
+        tableView.dataSource = self
+        tableView.delegate = self
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    var data = currTutor.classes
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        print("stuff 1")
+        print(data)
+        return 1
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print("stuff 2")
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "classCell", for: indexPath)
+        cell.textLabel?.text = data[indexPath.row]
+        print("stuff 3")
+        return cell
+    }
+    
 }
+
+
